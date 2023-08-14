d2fee4c9c52330fc3fbd7abddd9e79724427bba7
temogogitidze <temogogitidze@redberry.ge>
2023-08-14 23:27:50 +0400

    feat: add home and stat routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'notes',
+      component: ViewNotes
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/stats',
+      name: 'stats',
+      component: ViewStats
     }
   ]
 })
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="notes">
+    <h1>Notes</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+</script>
