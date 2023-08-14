d4d5dbe1e3ddc88b2973fbcc2cc43be392339977
temogogitidze <temogogitidze@redberry.ge>
2023-08-14 23:33:31 +0400

    refactor: move routes into separate variable

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,7 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <RouterLink to="/">Notes | </RouterLink>
+  <RouterLink to="/stats">Stats</RouterLink>
   <RouterView />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ViewNotes from '@/views/ViewNotes.vue'
 import ViewStats from '@/views/ViewStats.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'notes',
+    component: ViewNotes
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: ViewStats
+  }
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'notes',
-      component: ViewNotes
-    },
-    {
-      path: '/stats',
-      name: 'stats',
-      component: ViewStats
-    }
-  ]
+  routes
 })
 
 export default router
